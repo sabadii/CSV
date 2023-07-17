7f876c5c6b85089689790586b02271e9fa3bd19f
Neto test <sabaevadiana.ds@gmail.com>
2023-07-17 12:57:30 +0300

    adding the XML-JSON

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,22 +6,34 @@ import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TransformerException, ParserConfigurationException {
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
 
         // Создаем запись
         String[] employee1 = "1,John,Smith,USA,25".split(",");
-        String[] employee2 = "2,Inav,Petrov,RU,23".split(",");
+        String[] employee2 = "2,Ivan,Petrov,RU,23".split(",");
 
         // Создаем экземпляр CSVWriter
         try (CSVWriter writer = new CSVWriter(new FileWriter("data.csv"))) {
@@ -36,6 +48,47 @@ public class Main {
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json, "data.json");
+
+        List<Employee> list1 = parseXML("data.xml");
+        String json1 = listToJson(list1);
+        writeString(json1, "data2.json");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory. newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+        Element staff = document.createElement( "staff");
+        document.appendChild(staff);
+
+        Element employee = document.createElement("employee");
+        staff.appendChild(employee);
+
+        Element id = document.createElement("id");
+        id.appendChild(document.createTextNode("1"));
+        employee.appendChild(id);
+
+        Element firstname = document.createElement( "firstname");
+        employee.appendChild(firstname);
+        firstname.appendChild(document.createTextNode("John"));
+
+        Element lastname = document.createElement("lastname");
+        employee.appendChild(lastname);
+        lastname.appendChild(document.createTextNode("Smith"));
+
+        Element country = document.createElement("country");
+        employee.appendChild(country);
+        country.appendChild(document.createTextNode("USA"));
+
+        Element age = document.createElement("age");
+        employee.appendChild(age);
+        age.appendChild(document.createTextNode("25"));
+
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult( new File("data.xml"));
+
+        TransformerFactory transformerFactory = TransformerFactory. newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(domSource, streamResult);
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -59,7 +112,6 @@ public class Main {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         Type listType = new TypeToken<List<Employee>>() {}.getType();
         return gson.toJson(list, listType);
-
     }
 
     public static void writeString(String str, String fileName) {
@@ -68,5 +120,37 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public static List<Employee> parseXML(String filePath) {
+        List<Employee> list1 = new ArrayList<>();
+
+        try {
+            File file = new File(filePath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(file);
+
+            document.getDocumentElement().normalize();
+
+            NodeList nodeList = document.getDocumentElement().getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstname = element.getElementsByTagName("firstname").item(0).getTextContent();
+                    String lastname = element.getElementsByTagName("lastname").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                    Employee employee = new Employee(id, firstname, lastname, country, age);
+                    list1.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list1;
     }
 }
